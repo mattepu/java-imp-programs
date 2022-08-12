7292236aee2a35e0fb4bdefed930ffbf506b0021
mattepm <mahesh.mattepu@ig.com>
2022-08-12 14:56:15 +0100

    pushing another files

--- com.simple.java.programs/src/com/java8/examples/collections/Java8ConvertLowerCaseToUpperCase.java
+++ com.simple.java.programs/src/com/java8/examples/collections/Java8ConvertLowerCaseToUpperCase.java
@@ -1,2 +1,15 @@
-package com.java8.examples.collections;public class Java8ConvertLowerCaseToUpperCase {
+package com.java8.examples.collections;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class Java8ConvertLowerCaseToUpperCase {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("a", "b", "c", "d");
+
+        List<String> collect = strings.stream().map(element -> element.toUpperCase(Locale.ROOT)).collect(Collectors.toList());
+        System.out.println(collect);
+    }
 }
--- com.simple.java.programs/src/com/java8/examples/collections/Java8StreamBuilderMethod.java
+++ com.simple.java.programs/src/com/java8/examples/collections/Java8StreamBuilderMethod.java
@@ -1,2 +1,10 @@
-package com.java8.examples.collections;public class Java8StreamBuilderMethod {
+package com.java8.examples.collections;
+
+import java.util.stream.Stream;
+
+public class Java8StreamBuilderMethod {
+    public static void main(String[] args) {
+        Stream<String> stringBuilder = Stream.<String>builder().add("a").add("b").build();
+        stringBuilder.forEach(System.out::println);
+    }
 }
